0e33a24f4552ed3b91ebd5feffd76835e6016fca
jamie-legg <legg.jamie@gmail.com>
2020-03-25 22:45:24 +0000

    fiyesta addition

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+
 import './ExploreContainer.css';
+import { IonContent, IonItem, IonLabel, IonList, IonListHeader, IonSelect, IonSelectOption, IonPage, IonItemDivider } from '@ionic/react';
+import React, { useState } from 'react';
+
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+
+  const [fiyesta, setFiyesta] = useState<string>();
+
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonList>
+          <IonListHeader>
+            <IonLabel>
+              Which FiYESta have you been on?
+            </IonLabel>
+          </IonListHeader>
+
+          <IonItem>
+            <IonLabel>FiYESta</IonLabel>
+            <IonSelect value={fiyesta} placeholder="Select One" onIonChange={e => setFiyesta(e.detail.value)}>
+              <IonSelectOption value="rotterdam">Rotterdam</IonSelectOption>
+              <IonSelectOption value="hamburg">Hamburg</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItemDivider>Your Selections</IonItemDivider>
+          <IonItem>FiYESta: {fiyesta ?? '(none selected)'}</IonItem>
+        </IonList>
     </div>
   );
 };
